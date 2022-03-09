32ac65d16115238f2183082e341ea88cf1b9caf9
rwclutterbuck <rwclutterbuck@outlook.com>
2022-03-09 10:44:21 +0000

    Add: cases to searchReducers

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -6,6 +6,12 @@ const initState = {
 
 const searchReducer = (state = initState, action) => {
   switch (action.type) {
+    case "LOADING":
+      return { ...state, gitUser: action.payload, loading:true};
+    case "LOAD_RESULT":
+      return { ...state, result: action.payload, loading: false, error: false };
+    case "SET_ERROR":
+      return { ...state, error: action.payload, loading: false};
     default:
       return state;
   }
