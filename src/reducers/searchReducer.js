6f26bf26bd26801ef0ba1b49548ff5c8d0302fa9
rwclutterbuck <rwclutterbuck@outlook.com>
2022-03-09 10:11:23 +0000

    Add: redux files

---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+  gitUser: "",
+  result: {},
+  loading: false,
+};
+
+const searchReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default searchReducer;
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import { searchReducer } from "./reducers";
+
+const store = createStore(
+  searchReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
