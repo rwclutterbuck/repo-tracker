bdaeb0ed2e5b54a04b807b994eb298a96e0cf311
r0neel <ronbhagwakar@gmail.com>
2022-03-09 17:58:30 +0000

    Add: Repository table on profile load

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 
-export default function ({ result }) {
+export default function renderResult({ result }) {
+  const renderList = () => {
+    // console.log(Object.values(result.repos));
+    const repoObjectArray = [];
+    for (const property in result.repos) {
+      const entry = Object.values(result.repos)[property];
+      repoObjectArray.push({ name: entry.name, forks: entry.forks });
+    }
+
+    return (
+      <table>
+        <tr>
+          <th>Repository Name</th>
+          <th>Number of Forks</th>
+        </tr>
+        {repoObjectArray.map((repo) => (
+          <tr>
+            <td>{repo.name}</td>
+            <td>{repo.forks}</td>
+          </tr>
+        ))}
+      </table>
+    );
+  };
+
   return (
     <>
       <section>
-        <div>
+        <div id="avatar">
           <img width="128" height="128" src={result.avatar} />
         </div>
       </section>
+      <section>{renderList()}</section>
     </>
   );
 }
