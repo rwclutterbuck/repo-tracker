39a56ccad8474d94c392cb9f0acc0b47f111a431
r0neel <ronbhagwakar@gmail.com>
2022-03-09 10:56:18 +0000

    Add: boiler-plate Home and About pages

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>HELLO AND WELCOME TO OUR BRILLIANT GITHUB CLONE</h1>
+        <h2>Probably some more information I guess </h2>
+      </div>
+    </>
+  );
+};
+export default Home;
